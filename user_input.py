3c9a990a1dea0dda2dcb7b0b34a32d388eee7460
Sridhar Cuddalore Parthasarathy <scp392@nyu.edu>
2018-04-04 00:23:39 -0400

    input_check_validity_added

--- user_input.py
+++ user_input.py
@@ -4,15 +4,25 @@ from std_msgs.msg import String
 
 def talker():
 
+	accepted = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	demo_publisher = rospy.Publisher("DEMO", String, queue_size = 10)
 	rate = rospy.Rate(10)
 
 	while not rospy.is_shutdown():
+		valid = True
+		print ("")
 		DEMO = input("Enter the formation you would like to see : ")
-		demo_publisher.publish(DEMO)
+		for x in DEMO:
+			if x not in accepted:
+				print ("Please enter characters without space in caps !!")
+				print ("")
+				valid = False
+		if valid:		
+			demo_publisher.publish(DEMO)
 		rate.sleep()
 
 if __name__== '__main__':
+
 
 	rospy.init_node("user_input",anonymous = True)
 	try:
